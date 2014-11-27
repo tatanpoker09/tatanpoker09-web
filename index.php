ea444e62908de69c28bfb1c8533da2afecaf41ad
tatanpoker09 <tatanpoker09@PokerOS.com>
2014-11-26 21:30:34 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Taller HTML5</title>
+	<meta charset="utf-8" />
+
+	<!-- Esto se utiliza para que los navegadores reconoscan las etiquetas html5 -->
+	<!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- 
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	-->
+<!-- Estilos -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+ 	<!-- Contenido General -->
+ 	<div id="contenedor">
+ 	<div id="headerTop">
+ 	  <div id="contRedes">
+ 	  	<div class="twitter"></div> 
+ 	  	<div class="youtube"></div> 
+ 	  </div>
+ 	  <div id="contLogo">
+ 	  	<div class="logo"></div>
+ 	  </div>
+ 	  <div id="contMenu">
+ 	  	<div id="menu">
+ 	  			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Videos</a></li>
+					<li><a href="#">Mapas</a></li>
+					<li><a href="#">Plugins</a></li>
+					<li><a href="#">Solicitudes</a></li>
+					<li><a href="#">Contacto</a></li>
+				</ul>
+			</nav>
+ 	  	</div>
+ 	  </div>
+
+ 	</div>
+ 	
+		
+	</div>
+
+</body>
+</html>
